2803bd97812707003eefe481841cc14cec384192
MateoUgarte <>
2021-05-14 01:36:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let diameter = 0
+let userInput = 0
+
+document.getElementById('diameter').addEventListener ('click', area)
+
+// Math function area
+  function area () {
+  userInput = document.getElementById('input').value
+  userInput = parseInt(number)
+   number = 2 * Math.PI * r^2
+   document.getElementById('input').innerHTML = 'number = 2 * Math.PI * r^2' 
+    }
+
+document.getElementById('diameter').addEventListener ('click', circumfrence)
+// Math function circumfrence
+ function circumfrence () {
+  userInput = document.getElementById('input').value
+  userInput = parseInt (number)
+  number = (Math.PI) * r^2
+   document.getElementById('input').innerHTML = 'number = (Math.PI) * r^2' 
+    }
